c9fcf29a3251e48569110b2d748037e4ab145763
KoreanJungYJ <jung22337@naver.com>
2017-08-27 23:33:25 +0900

    Working Stopped

--- Ziptube/Ziptube.js
+++ Ziptube/Ziptube.js
@@ -1,65 +1,67 @@
-//  chrome.storage.sync.get(function(data) {
-//     document.getElementById('urlPart').innerText = data.value;
-//  })
+//storage get data example
 
-// document.getElementById('addBtn').addEventListener('click', () => {
-//     alert("check");
-//     var foo = document.getElementById('urlPart');
-//     foo.innerText = "test";
-//     var soo = foo.innerText;
-//     chrome.storage.sync.set({
-//         "value" : soo
-//     })
+/*chrome.storage.sync.get(function (data) {
+    var userData = document.querySelector('input');
+    userData.value = data.saveVal;
+});
 
-//set해서 get으로 값 가져오기 성공
-// })
-// document.addEventListener('DOMContentLoaded', function() {
-//     //  chrome.storage.sync.get(function (data) {
-//     //      var userData = data.saveVal;
-//     //      var foo = document.getElementsByClassName('urlTitles');
-//     //      foo[0].value = userData;
-//     //  });
+var addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', () => {
+    var input = document.querySelector('input');
+    var saveVal = input.value;
+    chrome.storage.sync.set({
+        'saveVal' : saveVal
+    });
+});*/
 
-//     var addBtn = document.getElementById('addBtn');
-//     addBtn.addEventListener('click', function() {
-//         var addCnt = 0;
-//         var showPart = document.getElementById('urlCover');
-//         var mainForm =
-//             '<label><input type="checkbox" class="checkBoxes" id="check'+addCnt+'"><input type="url" class="urlTitles" id="sepUrl'+addCnt+'" value="" maxlength = "40"></label>';
+/* x 이미지를 눌렀을 때 없애기 */
+var exitBtn = document.getElementById('exitImg');
+exitBtn.addEventListener('click', () => {
+    window.close();
+});
+
+/* data가져와서 설정하기 */
+chrome.storage.sync.get(function (data) {
+    
+});
+
+/* 추가 했을 때 관련 모든 로직 */
+var addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', () => {
+
+    //다운로드 버튼 표시하기
+    document.getElementById('downloadPart').style.display = "block";
+
+    //input 부분 표시하기
+    var addCnt = 0;
+    var showPart = document.getElementById('urlCover');
+    var mainForm =
+    '<label><input type="checkbox" class="checkBoxes" id="check'+addCnt+'"><input type="url" class="urlTitles" id="sepUrl'+addCnt+'" value="" maxlength = "40"></label>';
        
-//         var addDiv = document.createElement('div');
-//         addDiv.setAttribute("id", "keyForm" + addCnt);
-//         addDiv.innerHTML = mainForm;
-//         showPart.appendChild(addDiv);
-//         addCnt++;
+    var addDiv = document.createElement('div');
+    addDiv.setAttribute("id", "keyForm" + addCnt);
+    addDiv.innerHTML = mainForm;
+    showPart.appendChild(addDiv);
 
-//         chrome.tabs.getSelected(null, function(tabs){
-//             var userVal = document.getElementsByClassName('urlTitles');
-//             var pageUrl = tabs.url;
-//             var saveVal;
+    //url 받아와서 저장하기
+    chrome.tabs.getSelected(null, function(tabs){
+        var userVal = document.getElementsByClassName('urlTitles');
+        var pageUrl = tabs.url;
+        var saveVal, urlCnt;
 
-//             for(let cnt = 0; cnt < addCnt; cnt++){
-//                 userVal[cnt].value = pageUrl;
-//                 saveVal = userVal[cnt].value;
+        for(urlCnt = 0; urlCnt < userVal.length; urlCnt++){
+            userVal[urlCnt].value = pageUrl;
+            saveVal = userVal[urlCnt].value;
+            console.log(saveVal);
 
-//                 chrome.storage.sync.set({
-//                     'saveVal' : saveVal
-//                 });
-//             }
-//         });
-//     })
-// })
+            chrome.storage.sync.set({
+                'saveVal' : saveVal
+            });
+        }
+    });
 
-// chrome.storage.sync.get(function (data) {
-//     var userData = document.querySelector('input');
-//     userData.value = data.saveVal;
-// });
+    addCnt++;
+});
 
-// var addBtn = document.getElementById('addBtn');
-// addBtn.addEventListener('click', () => {
-//     var input = document.querySelector('input');
-//     var saveVal = input.value;
-//     chrome.storage.sync.set({
-//         'saveVal' : saveVal
-//     });
-// });
+
+
